27c4066199c0bc34df3edcb24aeb5a551bb1d20d
NhatThai <thaiquangnhatit@gmail.com>
2022-08-30 16:50:07 +0700

    Init Core

---
+++ NET6.Microservice.Core/PathBases/PathBaseStartupFilter.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.Options;
+
+namespace NET6.Microservice.Core.PathBases
+{
+    public class PathBaseStartupFilter : IStartupFilter
+    {
+        private readonly string _pathBase;
+
+        // Takes an IOptions<PathBaseSettings> instead of a string directly
+        public PathBaseStartupFilter(IOptions<PathBaseSettings> options)
+        {
+            _pathBase = options.Value.ApplicationPathBase;
+        }
+
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return app =>
+            {
+                app.UsePathBase(_pathBase);
+                next(app);
+            };
+        }
+    }
+
+}
